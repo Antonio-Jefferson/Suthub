d839e817b9d0135784eb9c46215c2ae77a74e94c
Antonio Jefferson <antjeffersonbatista@gmail.com>
2024-06-03 20:10:33 -0400

    chore: setup Nuxt 3 project with ESLint and Prettier

---
+++ nuxt.config.ts
@@ -0,0 +1,9 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: ["@nuxt/ui", '@nuxtjs/eslint-module'],
+  eslint: {
+    cache: false,
+    fix: true,
+  },
+})
