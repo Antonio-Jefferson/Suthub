4e3b397ebcc3c0c6c8c7f3582fec9ac96790259d
Antonio Jefferson <antjeffersonbatista@gmail.com>
2024-06-04 22:34:16 -0400

    feat: add footer component

---
+++ components/Footer.vue
@@ -0,0 +1,36 @@
+<template>
+  <div
+    class="fixed bottom-0 left-0 right-0 flex flex-col sm:flex-row justify-between items-center bg-footer w-full h-auto sm:h-20 p-4"
+  >
+    <div class="w-32 mb-4 sm:mb-0">
+      <img src="/assets/images/logo-suthub-white.png" alt="Logo" />
+    </div>
+    <p class="text-white mb-4 sm:mb-0">
+      Copyright © 2024. Todos os direitos reservados.
+    </p>
+    <div class="flex gap-2">
+      <img
+        src="/assets/images/Instagram.png"
+        alt="Logo Instagram"
+        class="h-6 w-6 sm:h-full sm:w-auto object-contain"
+      />
+      <img
+        src="/assets/images/Facebook.png"
+        alt="Logo Facebook"
+        class="h-6 w-6 sm:h-full sm:w-auto object-contain"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppFooter',
+};
+</script>
+
+<style scoped>
+.bg-footer {
+  background-color: #282727;
+}
+</style>
---
+++ layouts/register-layout.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="p-6 min-h-screen w-full bg-white sm:h-full sm:mb-28 md:mb-0">
+    <slot></slot>
+    <Footer />
+  </div>
+</template>
