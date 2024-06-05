b6012cf5114aca3e73b44c02d5c620c4bb9c3b7b
Antonio Jefferson <antjeffersonbatista@gmail.com>
2024-06-04 22:33:07 -0400

    chore: config tailwind css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/ui", '@nuxtjs/eslint-module'],
+  modules: ['@nuxt/ui', '@nuxtjs/eslint-module', '@nuxtjs/tailwindcss'],
   eslint: {
     cache: false,
     fix: true,
   },
-})
+  tailwindcss: {
+    cssPath: ['~/assets/css/tailwind.css', { injectPosition: 'first' }],
+    configPath: 'tailwind.config',
+    exposeConfig: {
+      level: 2,
+    },
+    config: {},
+    viewer: true,
+  },
+});
---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,vue,ts}',
+    './components/**/*.{js,vue,ts}',
+    './layouts/**/*.{js,vue,ts}',
+    './plugins/**/*.{js,ts}',
+    './nuxt.config.{js,ts}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        footer: '#282727',
+        secundary: '#071442',
+        'navigation-line': '#F60A0A',
+        button: '#4A72FF',
+        white: '#FFFFFF',
+        grees: {
+          DEFAULT: '#5F646D',
+          50: '#FFFFFF',
+          100: '#F4F5F6',
+          200: '#DEE0E3',
+          300: '#C8CBD0',
+          400: '#B3B6BD',
+          500: '#9DA2AA',
+          600: '#878D97',
+          700: '#727883',
+          800: '#5F646D',
+          900: '#40434A',
+          950: '#313338',
+        },
+      },
+    },
+  },
+  plugins: [],
+};
