103c83b6c1a8edcf241cdda14859ed7463a9a4ae
Antonio Jefferson <antjeffersonbatista@gmail.com>
2024-06-05 12:35:53 -0400

    feat: add validation rules to form fields

--- components/Form.vue
+++ components/Form.vue
@@ -1,30 +1,35 @@
 <template>
-  <div class="flex justify-center items-center min-h-screen">
+  <div class="flex justify-center items-center min-h-screen z-10">
     <UContainer class="w-full md:w-[800px] h-auto md:h-[700px] p-0">
       <UCard>
-        <h1 class="text-center font-bold text-xl">Formulário de Cadastro</h1>
+        <h1 class="text-center font-bold text-xl">
+          Formulário de Cadastro
+        </h1>
         <div class="m-6 flex items-center flex-wrap justify-center">
           <div v-for="stepNumber in 3" :key="stepNumber" class="flex items-center">
             <div
-              :class="{ 'bg-green-500': step >= stepNumber, 'bg-gray-400': step < stepNumber }"
+              :class="{ 'bg-green-500': step >= stepNumber, 'bg-gray-300': step < stepNumber }"
               class="md:h-12 w-12 flex justify-center items-center rounded-full"
             >
               {{ stepNumber }}
             </div>
-            <div :class="{ 'bg-green-500': step >= stepNumber, 'bg-gray-400': step < stepNumber }" v-if="stepNumber < 3"  class=" w-12 sm:w-28 md:w-56 h-1 bg-slate-50 my-2 md:my-0"></div>
+            <div v-if="stepNumber < 3" :class="{ 'bg-green-500': step >= stepNumber, 'bg-gray-300': step < stepNumber }" class="w-12 sm:w-28 md:w-56 h-1 bg-slate-300 my-2 md:my-0"></div>
           </div>
         </div>
-        <UForm :state="state" class="space-y-4" @submit="onSubmit">
-          <div  v-if="step === 1">
+        <UForm :schema="RegisterSchema" :state="state" class="space-y-4" @submit.prevent="onSubmit">
+          <div v-if="step === 1">
             <UFormGroup class="mb-6" label="Nome completo" required>
-              <UInput v-model="state.fullName"></UInput>
+              <UInput type="text" v-model="state.fullName" />
+              <div v-if="errors.fullName" class="text-red-500">{{ errors.fullName }}</div>
             </UFormGroup>
             <div class="flex flex-col md:flex-row md:gap-3">
               <UFormGroup label="Data de nascimento" required class="w-full mb-6 md:w-1/2">
-                <UInput v-model="state.dateOfBirth"></UInput>
+                <UInput type="date" v-model="state.dateOfBirth" />
+                <div v-if="errors.dateOfBirth" class="text-red-500">{{ errors.dateOfBirth }}</div>
               </UFormGroup>
               <UFormGroup label="CPF" required class="w-full mb-6 md:w-1/2">
-                <UInput v-model="state.CPF" ></UInput>
+                <UInput type="cpf" v-model="state.CPF" />
+                <div v-if="errors.CPF" class="text-red-500">{{ errors.CPF }}</div>
               </UFormGroup>
             </div>
             <div class="flex flex-col md:flex-row md:gap-3">
@@ -34,12 +39,14 @@
                   :options="pets"
                   @change="updateBreeds"
                 />
+                <div v-if="errors.petSpecies" class="text-red-500">{{ errors.petSpecies }}</div>
               </UFormGroup>
               <UFormGroup label="Raça do pet" class="w-full md:w-1/2">
                 <UInputMenu
                   v-model="state.petBreed"
                   :options="breeds"
                 />
+                <div v-if="errors.petBreed" class="text-red-500">{{ errors.petBreed }}</div>
               </UFormGroup>
             </div>
           </div>
@@ -47,36 +54,55 @@
           <div v-if="step === 2">
             <div class="flex flex-col md:flex-row md:gap-3">
               <UFormGroup label="CEP" class="w-full mb-6 md:w-1/2">
-                <UInput v-model="state.CEP"/>
+                <UInput type="cep" v-model="state.CEP" />
+                <div v-if="errors.CEP" class="text-red-500">{{ errors.CEP }}</div>
               </UFormGroup>
               <UFormGroup label="Estado" class="w-full mb-6 md:w-1/2">
-                <UInput v-model="state.state" />
+                <UInput type="text" v-model="state.state" />
+                <div v-if="errors.state" class="text-red-500">{{ errors.state }}</div>
               </UFormGroup>
             </div>
             <div class="flex flex-col md:flex-row md:gap-3">
               <UFormGroup label="Cidade" class="w-full mb-6 md:w-1/2">
-                <UInput v-model="state.city"/>
+                <UInput type="text" v-model="state.city" />
+                <div v-if="errors.city" class="text-red-500">{{ errors.city }}</div>
               </UFormGroup>
               <UFormGroup label="Bairro" class="w-full mb-6 md:w-1/2">
-                <UInput v-model="state.neighborhood" />
+                <UInput type="text" v-model="state.neighborhood" />
+                <div v-if="errors.neighborhood" class="text-red-500">{{ errors.neighborhood }}</div>
+              </UFormGroup>
+              <UFormGroup label="Rua" class="w-full mb-6 md:w-1/2">
+                <UInput type="text" v-model="state.street" />
+                <div v-if="errors.street" class="text-red-500">{{ errors.street }}</div>
               </UFormGroup>
             </div>
             <div class="flex flex-col md:flex-row md:gap-3">
-              <UFormGroup label="Rua" class="w-full mb-6 md:w-1/2">
-                <UInput v-model="state.street"/>
+              <UFormGroup label="Renda mensal" class="w-full mb-6 md:w-1/2">
+                <UInput type="number" v-model="state.monthlyIncome" />
               </UFormGroup>
             </div>
           </div>
 
           <div v-if="step === 3">
-            <div class="text-center">Cadastro realizado com sucesso!</div>
+            <div class="text-center">
+              Cadastro realizado com sucesso!
+            </div>
             <div class="flex justify-center">
-              <UButton class="mt-4">Ver dados cadastrados</UButton>
+              <UButton class="mt-4">
+                Ver dados cadastrados
+              </UButton>
             </div>
           </div>
           <div class="flex justify-end">
-            <UButton v-if="step > 1" class="mr-4" @click="previousStep">Voltar</UButton>
-            <UButton @click="nextStep" >Próximo</UButton>
+            <UButton v-if="step > 1" class="mr-4 cursor-pointer" @click="previousStep">
+              Voltar
+            </UButton>
+            <UButton v-if="step < 2" class="cursor-pointer" @click="nextStep">
+              Próximo
+            </UButton>
+            <UButton v-if="step === 2" class="cursor-pointer" type="submit">
+              Cadastrar
+            </UButton>
           </div>
         </UForm>
       </UCard>
@@ -84,57 +110,72 @@
   </div>
 </template>
 
-
-
 <script lang="ts" setup>
+import { z } from 'zod';
 import { reactive, ref } from 'vue';
+import { RegisterSchema } from '../schemas/RegisterSchema';
+import { isStepValid } from '../utils/isStepValid';
+
 const pets = ['cão', 'gato'];
 
 const breedsDogs = [
-  "Labrador Retriever",
-  "Golden Retriever",
-  "German Shepherd",
-  "Beagle",
-  "Bulldog",
-  "Outro"
+  'Labrador Retriever',
+  'Golden Retriever',
+  'German Shepherd',
+  'Beagle',
+  'Bulldog',
+  'Outro'
 ];
 const breedsCats = [
-  "Persian",
-  "Maine Coon",
-  "Siamese",
-  "Ragdoll",
-  "British Shorthair",
-  "Outro"
+  'Persa',
+  'Maine Coon',
+  'Siamese',
+  'Ragdoll',
+  'British Shorthair',
+  'Outro'
 ];
 
-const state = reactive({
+const state = reactive<z.output<typeof RegisterSchema>>({
   fullName: '',
   dateOfBirth: '',
   CPF: '',
-  petSpecies: '',
+  petSpecies: 'cão',
   petBreed: '',
   CEP: '',
   state: '',
   city: '',
   neighborhood: '',
   street: '',
-  monthlyincome: ''
+  monthlyIncome: ''
 });
+
+const errors = reactive<{ [key: string]: string }>({});
 
 function onSubmit(event: Event) {
   event.preventDefault();
-  console.log(state);
+  if (isStepValid(step, state, errors)) {
+    if (step.value === 2) {
+      console.log('Form submitted:', state);
+      step.value++;
+    }
+  } else {
+    alert('Por favor, preencha todos os campos obrigatórios.');
+  }
 }
-const breeds = ref<string[]>([]);
+
+const breeds = ref<string[]>(breedsDogs);
 
 function updateBreeds() {
   breeds.value = state.petSpecies === 'cão' ? breedsDogs : state.petSpecies === 'gato' ? breedsCats : [];
 }
+
 const step = ref(1);
 
 function nextStep() {
-  if (step.value < 3) {
-    step.value++;
+  if (isStepValid(step, state, errors)) {
+    if (step.value < 2) {
+      step.value++;
+    }
   }
 }
 
@@ -144,5 +185,3 @@ function previousStep() {
   }
 }
 </script>
-
-<style lang="css" scoped></style>
--- layouts/register-layout.vue
+++ layouts/register-layout.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="relative p-0 min-h-screen w-full bg-secundary sm:h-full sm:p-6 sm:mb-28 md:mb-0 overflow-x-hidden">
-    <div class="hidden sm:block absolute inset-y-0 right-0 w-1/2">
+    <div class="hidden md:block absolute inset-y-0 right-0 w-1/2 z-0">
       <img src="@/assets/images/form-vetor.png" alt="Imagem de fundo" class="w-full h-full object-cover">
     </div>
-    <slot />
+    <div class="relative z-10">
+      <slot />
+    </div>
     <Footer />
   </div>
 </template>
